4f177516f89de7d086424ab00f2e4da383b3da02
Mick Timony <mick.timony+github.com@gmail.com>
2018-12-08 00:06:12 -0500

    Initial basic proof of concept. Uses Mick's developement branch of squiggle (see the requirements file)

---
+++ squiggle_web/squiggle_views/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SquiggleViewsConfig(AppConfig):
+    name = 'squiggle_views'
---
+++ squiggle_web/squiggle_views/views.py
@@ -0,0 +1,32 @@
+import os
+
+from django.conf import settings
+from django.shortcuts import render, render_to_response
+from squiggle.cli import visualize_inner
+
+# a fasta file to use:
+fasta_file = str(
+        os.path.abspath(
+            os.path.join(
+                settings.BASE_DIR,
+                'static/example_seqs/human_HBB.fasta'
+                )
+            )
+        )
+
+# Create your views here.
+
+def index(request):
+    # the 'file' needs to be of type click.Path which is a tuple
+    file_tuple = (fasta_file,)
+    print("type(file_tuple): %s:%s" % (type(file_tuple),file_tuple))
+
+    script, div = visualize_inner(file_tuple, web=True)
+    pass
+    
+    return render_to_response(
+        'squiggle_views/squiggle.html',
+        {'script' : script , 'div' : div}
+    ) 
+
+
