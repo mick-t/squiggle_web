d843176f547b3b2e20e9e1f533139a7bcfcd18d0
Mick Timony <mick.timony+github.com@gmail.com>
2018-12-12 00:08:06 -0500

    home page: form now uploads a file to MEDIA_ROOT. Needs more work and error checking,for example, click upload when no file is selected.

--- squiggle_web/squiggle_web/views.py
+++ squiggle_web/squiggle_web/views.py
@@ -1,19 +1,29 @@
 """
-Top level views. used to render the top-level home page. 
+Top level views. used to render the top-level home page.
 
 TODO: is this the correct location for this view?
 
 """
+from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render
+
 
 def home(request):
     """
-    This function renders the home page. This is as basic as one can get
+    This function renders the home page and includes a form that allows file
+    uploads.
+
+    TODO: restrict uploads to fasta files.
+    TODO: needs error checking. Error if the user clicks upload and no file
+        selected.
     """
-    template = 'home.html'
-    
-    return render(request, template )
-    
-    
-    
-    
+    print("simple_upload")
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'home.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
+    return render(request, 'home.html')
